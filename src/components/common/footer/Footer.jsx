2de85e2ac6e52a5eef5e83e49f252db6c8b2a110
Eureka <eurekakabeya84@gmail.com>
2024-11-30 20:01:00 +0400

    footer date

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
         <div className='container'>
           <div className='box'>
             <div className='logo'>
-              <img src='../images/logo-light.png' alt='' />
+              <img src='../images/logoYouri2.png' alt='' />
               <h2>Do You Need Help With Anything?</h2>
               <p>Receive updates, hot deals, tutorials, discounts sent straignt in your inbox every month</p>
 
@@ -45,7 +45,7 @@ const Footer = () => {
         </div>
       </footer>
       <div className='legal'>
-        <span>© 2021 RentUP. Designd By GorkCoder.</span>
+        <span>&copy; {new Date().getFullYear()} GroupYknLLC. designed By Eureka Kabeya.</span>
       </div>
     </>
   )
